acfc95db89b5dff822bd30ee82e20abfaca048e8
faseeh1279 <faseehraza1279@gmail.com>
2024-11-09 14:09:26 +0500

    Added Common JSX Files

---
+++ src/components/RTE.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Editor } from '@tinymce/tinymce-react'
+import { Controller } from 'react-hook-form'
+
+const RTE = ({
+    name, control, label, defaultValue = ""
+}) => {
+    return (
+        <div className="w-full">
+            {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+            <Controller
+                name={name || "content"}
+                control={control}
+                render={({ field: { onChange } }) => (
+
+
+                    <Editor
+                        initialValue='default value'
+                        init={
+                            {
+                                branding: false,
+                                height: 500,
+                                menubar: true,
+                                plugins: [
+                                    'advlist autolink lists link image charmap print preview anchor',
+                                    'searchreplace visualblocks code fullscreen',
+                                    'insertdatetime media table paste code help wordcount'
+                                ],
+                                toolbar: [
+                                    'undo redo | formatselect | bold italic backcolor | \ alignleft aligncenter alignright alignjustify | \ bullist numlist outdent indent | removeformat | help'
+                                ]
+                            }
+
+
+                        }
+                    />
+                )}
+
+
+            />
+        </div>
+
+    )
+}
+
+export default RTE
